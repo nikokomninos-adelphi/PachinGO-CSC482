c7238e0baaa9254eccdf7aa2654f400b0eda7fab
Niko Komninos <nikolaoskomninos@mail.adelphi.edu>
2025-10-19 15:36:23 -0400

    fix: revert CORS changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,19 +31,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-//app.use(
-//  cors({
-//    origin: [
-//      "http://localhost:3000",
-//      "http://localhost:5173",
-//      "https://preview.construct.net",
-//      "https://pachingo.onrender.com",
-//      "https://playpachingo.vercel.app"
-//    ],
-//    credentials: true,
-//  })
-//);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://preview.construct.net",
+      "https://pachingo.onrender.com",
+      "https://playpachingo.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use("/api/v1", testRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/search", searchRoutes);
