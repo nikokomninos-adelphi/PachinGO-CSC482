a31000f71c88df0a4d2781395d4de3e1b4d68da5
Niko Komninos <nikolaoskomninos@mail.adelphi.edu>
2025-11-04 18:22:53 -0500

    fix: remove default behavior of arrow keys in game's iframe

--- frontend/app/routes/editor.tsx
+++ frontend/app/routes/editor.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/nav/Navbar";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useAuthStore } from "~/stores/useAuthStore";
 import { useNavigate } from "react-router";
 
@@ -11,10 +11,11 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const play = () => {
+const editor = () => {
   const { user } = useAuthStore();
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
     if (!user) navigate("/login");
@@ -26,6 +27,20 @@ const play = () => {
     window.scrollTo(0, document.body.scrollHeight);
   }, [isLoading]);
 
+  // Ensure that arrow keys do not follow
+  // normal browser behavior when game
+  // is focused
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    iframe?.contentWindow?.addEventListener("keydown", (e) => {
+      const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+      if (keys.includes(e.key)) e.preventDefault();
+    });
+
+    iframe?.addEventListener("click", () => iframe.contentWindow?.focus());
+    iframe?.addEventListener("mouseover", () => iframe.contentWindow?.focus());
+  });
+
   if (isLoading) return null;
 
   return (
@@ -34,7 +49,15 @@ const play = () => {
       <div className="bg-[url('/pattern2.svg')] bg-repeat animate-[scroll-pattern_100s_linear_infinite]">
         <div className="flex-1 tracking-tighter min-h-screen">
           <div className="flex min-h-screen justify-center items-center">
-            <iframe src="/game/index.html" width={1200} height={900} className="drop-shadow-2xl"/>
+            <iframe
+              ref={iframeRef}
+              src="/game/index.html"
+              width={1200}
+              height={900}
+              className="drop-shadow-2xl"
+              tabIndex={0}
+              allow="keyboard"
+            />
           </div>
         </div>
       </div>
@@ -42,4 +65,4 @@ const play = () => {
   );
 };
 
-export default play;
+export default editor;
--- frontend/app/routes/play.tsx
+++ frontend/app/routes/play.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/nav/Navbar";
-import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useAuthStore } from "~/stores/useAuthStore";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,13 +12,36 @@ export function meta({}: Route.MetaArgs) {
 }
 
 const play = () => {
-  const { id } = useParams();
+  const { user } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
-    localStorage.setItem("levelID", id!);
-    localStorage.setItem("layout", "Level Editor Online");
+    if (!user) navigate("/login");
+    setIsLoading(false);
+    localStorage.setItem("layout", "Level Editor");
+  }, [user]);
+
+  useEffect(() => {
     window.scrollTo(0, document.body.scrollHeight);
-  }, []);
+  }, [isLoading]);
+
+  // Ensure that arrow keys do not follow
+  // normal browser behavior when game
+  // is focused
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    iframe?.contentWindow?.addEventListener("keydown", (e) => {
+      const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+      if (keys.includes(e.key)) e.preventDefault();
+    });
+
+    iframe?.addEventListener("click", () => iframe.contentWindow?.focus());
+    iframe?.addEventListener("mouseover", () => iframe.contentWindow?.focus());
+  });
+
+  if (isLoading) return null;
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -25,7 +49,15 @@ const play = () => {
       <div className="bg-[url('/pattern2.svg')] bg-repeat animate-[scroll-pattern_100s_linear_infinite]">
         <div className="flex-1 tracking-tighter min-h-screen">
           <div className="flex min-h-screen justify-center items-center">
-            <iframe src="/game/index.html" width={1200} height={900} className="drop-shadow-2xl"/>
+            <iframe
+              ref={iframeRef}
+              src="/game/index.html"
+              width={1200}
+              height={900}
+              className="drop-shadow-2xl"
+              tabIndex={0}
+              allow="keyboard"
+            />
           </div>
         </div>
       </div>
