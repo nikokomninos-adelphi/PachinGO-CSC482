7119f3a1b206f19a65ca3d8c97a05f39516e12a8
Niko Komninos <nikolaoskomninos@mail.adelphi.edu>
2025-11-04 19:28:26 -0500

    fix: text wrap on user profile page

--- frontend/app/components/user/UserBox.tsx
+++ frontend/app/components/user/UserBox.tsx
@@ -20,12 +20,12 @@ const UserBox = ({
   });
 
   return (
-    <div className="flex flex-col justify-start items-start gap-5 border-1 border-[#E1E1EE] p-5 rounded-lg h-fit">
+    <div className="flex flex-col justify-start items-start gap-5 border-1 border-[#E1E1EE] p-5 rounded-lg h-fit w-60">
       <div className="flex justify-center items-center w-50 h-50 rounded-lg border-1 border-[#e1e1e1]">
         <p className="text-2xl">PFP</p>
       </div>
 
-      <h1 className="text-4xl">{username}</h1>
+      <h1 className="text-2xl break-words whitespace-normal w-full">{username}</h1>
 
       <div>
         <h2 className="text-sm">Member Since:</h2>
