0f6549f9540dca044458b3e57cd89adbbce20792
Niko Komninos <nikolaoskomninos@mail.adelphi.edu>
2025-11-16 01:18:03 -0500

    refactor: update look of user card

--- frontend/app/components/user/UserCard.tsx
+++ frontend/app/components/user/UserCard.tsx
@@ -3,7 +3,7 @@ const UserCard = ({ username }: { username: string }) => {
   return (
     <Link
       to={`/users/${username}`}
-      className="flex flex-row items-center w-60 h-15 p-2 border-1 border-[#e1e1e1] rounded-lg tracking-tight bg-[#fff] cursor-pointer hover:bg-[#fafafa] ease-linear duration-75"
+      className="flex flex-row items-center w-fit h-15 p-2 border-1 border-[#e1e1e1] rounded-lg tracking-tight bg-[#fff] cursor-pointer hover:bg-[#fafafa] ease-linear duration-75"
     >
       <div className="flex justify-center items-center w-10 h-10 rounded-lg border-1 border-[#e1e1e1] mr-3">
         <img src="/logo_small.png" />
