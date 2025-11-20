607bd243b56841388abae9db2a3fb441fb678570
Niko Komninos <nikolaoskomninos@mail.adelphi.edu>
2025-11-19 21:26:15 -0500

    fix: update game version on frontend

--- frontend/public/game/scripts/project/javaScriptInEvents.js
+++ frontend/public/game/scripts/project/javaScriptInEvents.js
@@ -18,170 +18,7 @@ const scriptsInEvents = {
 		runtime.callFunction("CheckLayout", layout);
 	},
 
-	async Gameplay_Event314_Act16(runtime, localVars)
-	{
-		// Gets the logged in user, and loads the previous state of the level editor peg layout
-		
-		runtime.globalVars.CustomerLevelAuthor = localStorage.getItem("user") || "PachinGO!";
-		
-		const pegDict = JSON.parse(runtime.globalVars.PegData).data;
-		
-		for (const key in pegDict) {
-		    const pegData = pegDict[key];
-		
-		    const newPeg = runtime.objects.Peg.createInstance("Pegs", pegData.x, pegData.y);
-		    newPeg.angle = pegData.angle;
-		    newPeg.setAnimation(pegData.animation);
-		}
-	},
-
-	async Gameplay_Event413_Act2(runtime, localVars)
-	{
-		const backgroundPicker = runtime.objects.ImageHere;
-		const backgroundPickerInst = backgroundPicker.getFirstInstance();
-		if (backgroundPickerInst) window.cachedBGFile = backgroundPickerInst.getFiles()[0];
-	},
-
-	async Gameplay_Event448_Act5(runtime, localVars)
-	{
-		const musicPicker = runtime.objects.MusicHere;
-		const musicPickerInst = musicPicker.getFirstInstance();
-		if (musicPickerInst) window.cachedMusicFile = musicPickerInst.getFiles()[0];
-	},
-
-	async Gameplay_Event487_Act4(runtime, localVars)
-	{
-		// Save the peg layout into a JSON
-		
-		const pegDict = {
-		    c2dictionary: true,
-		    data: {}
-		};
-		
-		for (const peg of runtime.objects.Peg.getAllInstances()) {
-		    const key = "Peg" + peg.uid;
-		
-		    if(peg.animationName === "CircleBlueActive") peg.setAnimation("CircleBlue");
-		    if(peg.animationName === "BrickBlueActive") peg.setAnimation("BrickBlue");
-		    if(peg.animationName === "CurveBlueActive") peg.setAnimation("CurveBlue");
-		
-		    const pegData = {
-		        x: peg.x,
-		        y: peg.y,
-		        angle: peg.angle,
-		        animation: peg.animationName
-		    };
-		
-		    //pegDict.data[key] = JSON.stringify(pegData);
-		    pegDict.data[key] = pegData;
-		}
-		
-		runtime.globalVars.PegData = JSON.stringify(pegDict);
-	},
-
-	async Gameplay_Event498_Act5(runtime, localVars)
-	{
-		// Save the peg layout into a JSON
-		
-		const pegDict = {
-		    c2dictionary: true,
-		    data: {}
-		};
-		
-		for (const peg of runtime.objects.Peg.getAllInstances()) {
-		    const key = "Peg" + peg.uid;
-		
-		    if(peg.hasTags("Master")) continue;
-		
-		    if(peg.animationName === "CircleBlueActive") peg.setAnimation("CircleBlue");
-		    if(peg.animationName === "BrickBlueActive") peg.setAnimation("BrickBlue");
-		    if(peg.animationName === "CurveBlueActive") peg.setAnimation("CurveBlue");
-		
-		    const pegData = {
-		        x: peg.x,
-		        y: peg.y,
-		        angle: peg.angle,
-		        animation: peg.animationName
-		    };
-		
-		    pegDict.data[key] = pegData;
-		}
-		
-		runtime.globalVars.PegData = JSON.stringify(pegDict);
-	},
-
-	async Gameplay_Event498_Act6(runtime, localVars)
-	{
-const pegs = JSON.parse(runtime.globalVars.PegData).data;
-
-const upload = async () => { 
-    const formData = new FormData();
-
-    formData.append("name", runtime.globalVars.CustomLevelName || "Custom Level");
-    formData.append("author", localStorage.getItem("user"));
-    formData.append("desc", runtime.globalVars.CustomLevelDesc || "This level is so fun!");
-    formData.append("pegLayout", runtime.globalVars.PegData);
-    formData.append("numOrange", runtime.globalVars.NumberOfOrangePegsInLevel);
-    formData.append("numBalls", runtime.globalVars.BallCountOnStart);
-    formData.append("backgroundImageOpacity", runtime.globalVars.BGIMageOpacity);
-    formData.append("backgroundImageHSL", JSON.stringify({ "H": runtime.globalVars.HBGColor, "S": runtime.globalVars.SBGColor, "L": runtime.globalVars.LBGColor }));
-    formData.append("musicSelect", runtime.globalVars.MusicSelect);
-    formData.append("wallHSL", JSON.stringify({ "H": runtime.globalVars.HWall, "S": runtime.globalVars.SWall, "L": runtime.globalVars.LWall }));
-    formData.append("scoreHSL", JSON.stringify({ "H": runtime.globalVars.HScore, "S": runtime.globalVars.SScore, "L": runtime.globalVars.LScore }));
-    formData.append("crystalHSL", JSON.stringify({ "H": runtime.globalVars.HCrystal, "S": runtime.globalVars.SCrystal, "L": runtime.globalVars.LCrystal }));
-
-    const backgroundPicker = runtime.objects.ImageHere;
-    const musicPicker = runtime.objects.MusicHere;
-
-    const backgroundPickerInst = backgroundPicker.getFirstInstance();
-    const musicPickerInst = musicPicker.getFirstInstance();
-
-    let backgroundFile = "";
-    let musicFile = "";
-
-    if (backgroundPickerInst) backgroundFile = backgroundPickerInst.getFiles()[0];
-    if (musicPickerInst) musicFile = musicPickerInst.getFiles()[0];
-
-    if(!backgroundFile) {
-        try {
-            backgroundFile = new File([window.cachedBGFile], window.cachedBGFile.name);
-        } catch (e) { console.error(e); }
-    } 
-
-    if(!musicFile) {
-        try {
-            musicFile = new File([window.cachedMusicFile], window.cachedMusicFile.name);
-        } catch (e) { console.error(e); }
-    } 
-
-    if (backgroundFile) {
-        formData.append("background", backgroundFile, backgroundFile.name);
-    }
-
-    if (musicFile) {
-        formData.append("music", musicFile, musicFile.name);
-    }
-
-    const res = await fetch(`${runtime.globalVars.BackendURL}/api/v1/level/uploadLevel`, {
-    method: "POST",
-    mode: "cors",
-    body: formData,
-    });
-
-    const data = await res.json();
-    res.ok ? runtime.globalVars.UploadStatus = 2 : runtime.globalVars.UploadStatus = 1;
-    if (res.ok) localStorage.setItem("levelID", data.levelID);
-}
-
-runtime.globalVars.BGIMageOpacity !== 0 ? await upload() : runtime.globalVars.UploadStatus = 1;
-	},
-
-	async Gameplay_Event501_Act8(runtime, localVars)
-	{
-		localStorage.setItem("uploaded", "true");
-	},
-
-	async Gameplay_Event17_Act4(runtime, localVars)
+	async Gameplay_Event40_Act4(runtime, localVars)
 	{
 // Load the level editor peg layout into the
 // gameplay test layout
@@ -259,6 +96,169 @@ if (runtime.layout.name === "Level Editor Online") {
         menu.destroy();
     } catch (e) { console.error(e); }
 }
+	},
+
+	async Gameplay_Event335_Act16(runtime, localVars)
+	{
+		// Gets the logged in user, and loads the previous state of the level editor peg layout
+		
+		runtime.globalVars.CustomerLevelAuthor = localStorage.getItem("user") || "PachinGO!";
+		
+		const pegDict = JSON.parse(runtime.globalVars.PegData).data;
+		
+		for (const key in pegDict) {
+		    const pegData = pegDict[key];
+		
+		    const newPeg = runtime.objects.Peg.createInstance("Pegs", pegData.x, pegData.y);
+		    newPeg.angle = pegData.angle;
+		    newPeg.setAnimation(pegData.animation);
+		}
+	},
+
+	async Gameplay_Event434_Act2(runtime, localVars)
+	{
+		const backgroundPicker = runtime.objects.ImageHere;
+		const backgroundPickerInst = backgroundPicker.getFirstInstance();
+		if (backgroundPickerInst) window.cachedBGFile = backgroundPickerInst.getFiles()[0];
+	},
+
+	async Gameplay_Event469_Act5(runtime, localVars)
+	{
+		const musicPicker = runtime.objects.MusicHere;
+		const musicPickerInst = musicPicker.getFirstInstance();
+		if (musicPickerInst) window.cachedMusicFile = musicPickerInst.getFiles()[0];
+	},
+
+	async Gameplay_Event508_Act3(runtime, localVars)
+	{
+		// Save the peg layout into a JSON
+		
+		const pegDict = {
+		    c2dictionary: true,
+		    data: {}
+		};
+		
+		for (const peg of runtime.objects.Peg.getAllInstances()) {
+		    const key = "Peg" + peg.uid;
+		
+		    if(peg.animationName === "CircleBlueActive") peg.setAnimation("CircleBlue");
+		    if(peg.animationName === "BrickBlueActive") peg.setAnimation("BrickBlue");
+		    if(peg.animationName === "CurveBlueActive") peg.setAnimation("CurveBlue");
+		
+		    const pegData = {
+		        x: peg.x,
+		        y: peg.y,
+		        angle: peg.angle,
+		        animation: peg.animationName
+		    };
+		
+		    //pegDict.data[key] = JSON.stringify(pegData);
+		    pegDict.data[key] = pegData;
+		}
+		
+		runtime.globalVars.PegData = JSON.stringify(pegDict);
+	},
+
+	async Gameplay_Event519_Act4(runtime, localVars)
+	{
+		// Save the peg layout into a JSON
+		
+		const pegDict = {
+		    c2dictionary: true,
+		    data: {}
+		};
+		
+		for (const peg of runtime.objects.Peg.getAllInstances()) {
+		    const key = "Peg" + peg.uid;
+		
+		    if(peg.hasTags("Master")) continue;
+		
+		    if(peg.animationName === "CircleBlueActive") peg.setAnimation("CircleBlue");
+		    if(peg.animationName === "BrickBlueActive") peg.setAnimation("BrickBlue");
+		    if(peg.animationName === "CurveBlueActive") peg.setAnimation("CurveBlue");
+		
+		    const pegData = {
+		        x: peg.x,
+		        y: peg.y,
+		        angle: peg.angle,
+		        animation: peg.animationName
+		    };
+		
+		    pegDict.data[key] = pegData;
+		}
+		
+		runtime.globalVars.PegData = JSON.stringify(pegDict);
+	},
+
+	async Gameplay_Event519_Act5(runtime, localVars)
+	{
+const pegs = JSON.parse(runtime.globalVars.PegData).data;
+
+const upload = async () => { 
+    const formData = new FormData();
+
+    formData.append("name", runtime.globalVars.CustomLevelName || "Custom Level");
+    formData.append("author", localStorage.getItem("user"));
+    formData.append("desc", runtime.globalVars.CustomLevelDesc || "This level is so fun!");
+    formData.append("pegLayout", runtime.globalVars.PegData);
+    formData.append("numOrange", runtime.globalVars.NumberOfOrangePegsInLevel);
+    formData.append("numBalls", runtime.globalVars.BallCountOnStart);
+    formData.append("backgroundImageOpacity", runtime.globalVars.BGIMageOpacity);
+    formData.append("backgroundImageHSL", JSON.stringify({ "H": runtime.globalVars.HBGColor, "S": runtime.globalVars.SBGColor, "L": runtime.globalVars.LBGColor }));
+    formData.append("musicSelect", runtime.globalVars.MusicSelect);
+    formData.append("wallHSL", JSON.stringify({ "H": runtime.globalVars.HWall, "S": runtime.globalVars.SWall, "L": runtime.globalVars.LWall }));
+    formData.append("scoreHSL", JSON.stringify({ "H": runtime.globalVars.HScore, "S": runtime.globalVars.SScore, "L": runtime.globalVars.LScore }));
+    formData.append("crystalHSL", JSON.stringify({ "H": runtime.globalVars.HCrystal, "S": runtime.globalVars.SCrystal, "L": runtime.globalVars.LCrystal }));
+
+    const backgroundPicker = runtime.objects.ImageHere;
+    const musicPicker = runtime.objects.MusicHere;
+
+    const backgroundPickerInst = backgroundPicker.getFirstInstance();
+    const musicPickerInst = musicPicker.getFirstInstance();
+
+    let backgroundFile = "";
+    let musicFile = "";
+
+    if (backgroundPickerInst) backgroundFile = backgroundPickerInst.getFiles()[0];
+    if (musicPickerInst) musicFile = musicPickerInst.getFiles()[0];
+
+    if(!backgroundFile) {
+        try {
+            backgroundFile = new File([window.cachedBGFile], window.cachedBGFile.name);
+        } catch (e) { console.error(e); }
+    } 
+
+    if(!musicFile) {
+        try {
+            musicFile = new File([window.cachedMusicFile], window.cachedMusicFile.name);
+        } catch (e) { console.error(e); }
+    } 
+
+    if (backgroundFile) {
+        formData.append("background", backgroundFile, backgroundFile.name);
+    }
+
+    if (musicFile) {
+        formData.append("music", musicFile, musicFile.name);
+    }
+
+    const res = await fetch(`${runtime.globalVars.BackendURL}/api/v1/level/uploadLevel`, {
+    method: "POST",
+    mode: "cors",
+    body: formData,
+    });
+
+    const data = await res.json();
+    res.ok ? runtime.globalVars.UploadStatus = 2 : runtime.globalVars.UploadStatus = 1;
+    if (res.ok) localStorage.setItem("levelID", data.levelID);
+}
+
+runtime.globalVars.BGIMageOpacity !== 0 ? await upload() : runtime.globalVars.UploadStatus = 1;
+	},
+
+	async Gameplay_Event522_Act8(runtime, localVars)
+	{
+		localStorage.setItem("uploaded", "true");
 	}
 };
 
